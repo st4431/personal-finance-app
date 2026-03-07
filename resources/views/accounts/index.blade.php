522f9624cfb9467f39b60c0f78049cc7f082c2aa
st <st20020815@icloud.com>
2026-03-07 11:39:30 +0900

    index.blade.phpを作成

---
+++ resources/views/accounts/index.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2>口座一覧</h2>
+    </x-slot>
+
+    <a href="{{ route('accounts.create') }}">新規口座登録</a>
+
+    @foreach ($accounts as $account)
+        <div>
+            <span>{{ $account->name }}</span>
+            <span>{{ $account->type }}</span>
+            <span>{{ $account->category }}</span>
+            <a href="{{ route('accounts.edit', $account->id) }}">編集</a>
+            <form method="POST" action="{{ route('accounts.destroy', $account->id) }}" style="display:inline">
+              @csrf
+              @method('DELETE')
+              <button type="submit">削除</button>
+            </form>
+        </div>
+    @endforeach
+</x-app-layout>
+

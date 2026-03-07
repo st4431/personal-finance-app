39538c214981735cae8b21c356cd58663a2e49c0
st <st20020815@icloud.com>
2026-03-07 11:17:09 +0900

    AccountControllerのindexメソッドを実装

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Account;
 
 class AccountController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    // Request $requestでHTTPリクエストの情報を引数として受け取っている
+    public function index(Request $request)
     {
-        //
+        // auth() は認証情報（全ユーザーのidやパスワードなど）にアクセスする
+        // id() 今ログインしているユーザーのidを取得する
+        $query = Account::where('user_id', auth()->id());
+
+        // 口座情報は多くても10個ぐらいが想定されるのでページネーションは不要と判断
+        $accounts = $query->get();
+
+        return view('accounts.index', ['accounts' => $accounts]);
     }
 
     /**
